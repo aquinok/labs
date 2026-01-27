42d7e645b10342494aa4224a2c5ab16bb9b498e1
Nicholas Guyer <guyern@gmail.com>
2026-01-27 17:08:33 -0600

    lab: Terraform added Route54 with roundrobin A record for cluster

---
+++ terraform/envs/lab/us-east-1/route53.tf
@@ -0,0 +1,49 @@
+variable "domain_name" {
+  type        = string
+  description = "Existing Route53 public hosted zone name (e.g. aquinok.net)"
+  default     = "aquinok.net"
+}
+
+# Look up the existing public hosted zone; we are NOT creating a new zone.
+data "aws_route53_zone" "public" {
+  name         = "${var.domain_name}."
+  private_zone = false
+}
+
+locals {
+  # Creates:
+  #   lab-01.aquinok.net -> <ip>
+  #   lab-02.aquinok.net -> <ip>
+  #   ...
+  node_a_records = {
+    for idx, ip in module.ec2.public_ips :
+    "${module.ec2.names[idx]}.${var.domain_name}" => ip
+  }
+}
+ 
+resource "aws_route53_record" "nodes_a" {
+  for_each = local.node_a_records
+
+  zone_id = data.aws_route53_zone.public.zone_id
+  name    = each.key
+  type    = "A"
+  ttl     = 60
+  records = [each.value]
+}
+
+# Optional: one service name that round-robins to all nodes (fine for lab)
+resource "aws_route53_record" "vault_rr" {
+  zone_id = data.aws_route53_zone.public.zone_id
+  name    = "vault.${var.domain_name}"
+  type    = "A"
+  ttl     = 60
+  records = module.ec2.public_ips
+}
+
+output "node_fqdns" {
+  value = [for n in module.ec2.names : "${n}.${var.domain_name}"]
+}
+
+output "vault_fqdn" {
+  value = "vault.${var.domain_name}"
+}
